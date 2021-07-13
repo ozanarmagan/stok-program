7fd97a4a91dab899de2b83cb520bc54841e6904d
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2021-07-13 20:04:35 +0300

    api

---
+++ frontend/API/bill_api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import {API_URL} from "./api_constant"
+
+
+export async function apiGetBilling(){
+    return axios.get(API_URL +"bills/")
+}
+
+export async function apiPostBilling(customer_id,products,created_date,pay_type,is_company){
+    return axios.post(API_URL +"login/",{
+        customer_id:customer_id,
+        products:products,
+        created_date:created_date,
+        pay_type:pay_type,
+        is_company:is_company})
+}
+
+export async function apiGetBillingById(id){
+    return axios.get(API_URL +"bills/"+id)
+}
