e81deaad0a80711b5731e0ac4d36558fa5d51489
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2021-07-17 16:55:54 +0300

    url

--- backend/constants/index.js
+++ backend/constants/index.js
@@ -1,4 +1,4 @@
-module.exports.MONGO_URL = "mongodb://vade:82eb6b8c99d1c3f9bb47d9e0ca1b9d2025561827822821a85167ee4f8a28b271@46.31.79.30:27067/lorect_stok?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+module.exports.MONGO_URL = "mongodb://vade:82eb6b8c99d1c3f9bb47d9e0ca1b9d2025561827822821a85167ee4f8a28b271@localhost:27067/lorect_stok?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 module.exports.SECRET_KEY = "=g@G9Z[RDk(@ !d+DnMR-masQ1z&pfPU{s!J#0XJyGz`K8<(@HB:.nh.NKxvU?tY";
 module.exports.REFRESH_KEY = "qA40%NK~SnxokS^#-:z436GU=Hv+@;_)DEWVdTFT[[/P(&RU$`CX1g_Q^(q=-nbE";
 module.exports.SECRET_EXPIRES_IN = '5h';
