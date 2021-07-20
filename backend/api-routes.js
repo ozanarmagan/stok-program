f3ff5f555e686e0fd4e6596c9338f4a43f2ba250
ozanarmagan <oza-armagan@yandex.com>
2021-07-21 00:36:21 +0300

    file upload update

--- backend/api-routes.js
+++ backend/api-routes.js
@@ -5,7 +5,9 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, 'images');
+        const path = `./images/`
+        fs.mkdirSync(path, { recursive: true })
+        return cb(null, path)
     },
     filename: function(req, file, cb) {   
         cb(file.originalname);
