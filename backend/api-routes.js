d26425829e58d7328c0063d68fab955a02b04402
ozanarmagan <oza-armagan@yandex.com>
2021-07-21 13:31:13 +0300

    fixx

--- backend/api-routes.js
+++ backend/api-routes.js
@@ -6,8 +6,10 @@ const multer = require('multer')
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
         const path = `./images`
+        console.log(req.file);
         fs.mkdirSync(path, { recursive: true })
         return cb(null, 'images/')
+
     },
     filename: function(req, file, cb) {   
         cb(file.originalname);
