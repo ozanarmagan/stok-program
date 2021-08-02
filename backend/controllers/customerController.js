b43492a86b6da07822e0363ce002e3a41416939c
ozanarmagan <oza-armagan@yandex.com>
2021-08-02 14:10:12 +0300

    backend customer update model + controller

--- backend/controllers/customerController.js
+++ backend/controllers/customerController.js
@@ -6,7 +6,7 @@ var User = require("../models/userModel");
 exports.index = function (req,res) {
     try
     {
-        var user = token.verifyToken(req.query.token,'token');
+        var user = token.verifyToken(req.query.token,'acceess');
         const { filter, skip, limit, sort, projection, population } = aqp(req.query,{blacklist:['token'],});
         Customer.find(filter)
         .skip(skip)
@@ -47,7 +47,7 @@ exports.index = function (req,res) {
 exports.edit = function (req,res) {
     try
     {
-        var user = token.verifyToken(req.body.token,'token');
+        var user = token.verifyToken(req.body.token,'access');
         try
         {
             Customer.findById(req.params.customer_id,function (error,customertoedit) {
@@ -55,6 +55,11 @@ exports.edit = function (req,res) {
                 customertoedit.address = req.body.address || customertoedit.address;
                 customertoedit.phone = req.body.phone || customertoedit.phone;
                 customertoedit.tax_no = req.body.tax_no || customertoedit.tax_no;
+                customertoedit.gsm = req.body.gsm || customertoedit.gsm;
+                customertoedit.state = req.body.state || customertoedit.state;
+                customertoedit.town = req.body.town || customertoedit.town;
+                customertoedit.country = req.body.country || customertoedit.country;
+                customertoedit.identity_number = req.body.identity_number || customertoedit.identity_number;
                 customertoedit.tax_place = req.body.tax_place || customertoedit.tax_place;
                 customertoedit.note = req.body.note || customertoedit.note;
                 customertoedit.debtlimit = req.body.debtlimit || customertoedit.debtlimit;
@@ -77,7 +82,7 @@ exports.edit = function (req,res) {
 exports.delete = async function(req,res) {
     try
     {
-        var user = token.verifyToken(req.body.token,'token');
+        var user = token.verifyToken(req.body.token,'access');
         Customer.deleteOne({_id:req.params.customer_id},(err) => { if(err) {res.json({status:400,message:"An error occured"})} res.json({status:200,message:"Customer has been deleted"})});
     }
     catch
@@ -89,12 +94,17 @@ exports.delete = async function(req,res) {
 exports.new = async function (req,res) {
     try 
     {
-        var user = token.verifyToken(req.body.token,'token');
+        var user = token.verifyToken(req.body.token,'access');
         var newcustomer = new Customer();
         newcustomer.name = req.body.name;
         newcustomer.address = req.body.address;
         newcustomer.phone = req.body.phone;
         newcustomer.tax_no = req.body.tax_no;
+        newcustomer.gsm = req.body.gsm;
+        newcustomer.state = req.body.state;
+        newcustomer.town = req.body.town;
+        newcustomer.country = req.body.country;
+        newcustomer.identity_number = req.body.identity_number;
         newcustomer.tax_place = req.body.tax_place;
         newcustomer.note = req.body.note;
         newcustomer.debtlimit = req.body.debtlimit;
@@ -115,7 +125,7 @@ exports.new = async function (req,res) {
 exports.view = async function (req,res) {
     try
     {
-        var user = token.verifyToken(req.query.token,'token');
+        var user = token.verifyToken(req.query.token,'access');
         try
         {
             var customer = Customer.findById(req.params.customer_id);
